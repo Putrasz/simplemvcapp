eb7dfbdce81b2b805fc6beea52e40e47d43acdfb
Hafidh Ghazy <hafidhgpf03@gmail.com>
2025-09-27 22:39:40 +0700

    update

--- app/views/siswa/detail.php
+++ app/views/siswa/detail.php
@@ -5,7 +5,8 @@
     <h5 class="card-title"><?=$data['siswa']['nama']?></h5>
     <h6 class="card-subtitle mb-2 text-body-secondary"><?=$data['siswa']['jurusan']?></h6>
     <p class="card-text mb-0">Email : <?=$data['siswa']['email']?></p>
-    <p class="card-text">Umur : <?=$data['siswa']['umur']?></p>
+    <p class="card-text mb-0">Tinggi Badan : <?=$data['siswa']['tinggi']?></p>
+    <p class="card-text mb-3">Umur : <?=$data['siswa']['umur']?></p>
     <a href="<?=BASEURL?>/public/siswa" class="card-link">Back</a>
   </div>
 </div>
